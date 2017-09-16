f6b5ed4489ff7706c9a34058d7fb1d8cf84d3dd9
reynau <xavi.lopez.reynau@gmail.com>
2017-09-16 19:34:51 +0200

    Add Lloyd relaxation

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,32 +5,29 @@ let basicVoronoi = {
   canvas: null,
   bbox: {xl:0, xr:800, yt:0, yb:600},
   points: [],
+  timeoutDelay: 100,
 
-  init: function (n, margin) {
-    if (margin === null || margin === undefined) margin = 0
-
-    this.margin = margin
+  init: function (n, r) {
     this.canvas = document.getElementById('voronoiCanvas')
-    this.generatePoints(n)
+    this.generatePoints(n, r)
   },
 
-  generatePoints: function (n) {
+  generatePoints: function (n, r) {
     let points = []
 
-    let xmargin = this.canvas.width * this.margin
-    let ymargin = this.canvas.height * this.margin
-    let xo = xmargin
-    let dx = this.canvas.width - xmargin*2
-    let yo = ymargin
-    let dy = this.canvas.height - ymargin*2
+    let dx = this.canvas.width
+    let dy = this.canvas.height
 
     for (let i = 0; i < n; ++i) {
-      let x = self.Math.round((xo+self.Math.random()*dx))
-      let y = self.Math.round((yo+self.Math.random()*dy))
+      let x = self.Math.round((self.Math.random()*dx))
+      let y = self.Math.round((self.Math.random()*dy))
       points.push({x: x, y: y});
     }
 
     this.compute(points)
+    while (r--) {
+      this.relaxPoints()
+    }
   },
 
   compute: function (points) {
@@ -38,6 +35,56 @@ let basicVoronoi = {
     this.voronoi.recycle(this.diagram)
     this.diagram = this.voronoi.compute(points, this.bbox)
     this.render()
+  },
+
+  relaxPoints: function () {
+    let cells = this.diagram.cells
+    let iCell = cells.length
+
+    let cell, cellCentroid
+    let newPoints = []
+    while (iCell--) {
+      cell = cells[iCell]
+      cellCentroid = this.cellCentroid(cell)
+      newPoints.push(cellCentroid)
+    }
+    this.compute(newPoints)
+  },
+
+  cellCentroid: function(cell) {
+    let x = 0
+    let y = 0
+    let halfedges = cell.halfedges
+    let iHalfedge = halfedges.length
+    let halfedge
+    let v, p1, p2
+    while (iHalfedge--) {
+      halfedge = halfedges[iHalfedge];
+      p1 = halfedge.getStartpoint();
+      p2 = halfedge.getEndpoint();
+      v = p1.x*p2.y - p2.x*p1.y;
+      x += (p1.x+p2.x) * v;
+      y += (p1.y+p2.y) * v;
+    }
+    v = this.cellArea(cell) * 6;
+    return {x: x/v,y: y/v};
+  },
+
+  cellArea: function(cell) {
+    let area = 0
+    let halfedges = cell.halfedges
+    let iHalfedge = halfedges.length
+    let halfedge
+    let p1, p2
+    while (iHalfedge--) {
+      halfedge = halfedges[iHalfedge]
+      p1 = halfedge.getStartpoint()
+      p2 = halfedge.getEndpoint()
+      area += p1.x * p2.y
+      area -= p1.y * p2.x
+    }
+    area /= 2
+    return area
   },
 
   render: function () {
